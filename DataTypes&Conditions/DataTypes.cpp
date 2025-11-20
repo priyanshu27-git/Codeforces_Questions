0739b83f3aab7cfb73d958ecae3d6af0273fa3d0
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-11-20 14:45:21 +0530

    first commit

---
+++ DataTypes&Conditions/DataTypes.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int INT ;
+    long long LONG ;
+    char CHAR ;
+    float FLOAT ;
+    double DOUBLE ;
+    cin >> INT >> LONG >> CHAR >> FLOAT >> DOUBLE;
+    cout << INT << endl; 
+    cout << LONG << endl;
+    cout << CHAR << endl;
+    cout << FLOAT << endl;
+    cout << DOUBLE << endl;
+}
