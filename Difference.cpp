ec04b1e50760bf84ed9785859dc51537f19db2e5
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-11-21 23:54:12 +0530

    deleted

--- Difference.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    long long A,B,C,D;
-    cin >> A >> B >> C >> D;
-    cout << "Difference = " << (A*B) - (C*D);
-}
--- SumOfOneToN.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    long long N;
-    cin >> N;
-    cout << (N * (N + 1)) / 2;
-}
