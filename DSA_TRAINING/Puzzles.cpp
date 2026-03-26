6035aefd828c6ed1f511b3212448f0683afab6d0
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-03-26 23:46:33 +0530

    next question

---
+++ DSA_TRAINING/Puzzles.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n,m;
+    cin >> n >> m;
+    int a[m];
+    for (int i = 0; i < m; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a , a + n);
+    
+    
+    return 0;
+}
