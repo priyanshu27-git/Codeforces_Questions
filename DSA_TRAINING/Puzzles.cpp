04bd87e8428fb1c48d40a019402bcf6270d15169
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-03-27 10:30:00 +0530

    Puzzles solved

--- DSA_TRAINING/Puzzles.cpp
+++ DSA_TRAINING/Puzzles.cpp
@@ -2,16 +2,28 @@
 #include <algorithm>
 using namespace std;
 
-int main() {
-    int n,m;
+int main()
+{
+    int n, m;
     cin >> n >> m;
     int a[m];
     for (int i = 0; i < m; i++)
     {
         cin >> a[i];
     }
-    sort(a , a + n);
-    
-    
+    sort(a, a + m);
+    int solu = 10000000, temp;
+
+    for (int i = 0; i < (m - n) + 1; i++)
+    {
+        temp = a[(i + n) - 1] - a[i];
+        if (temp < solu)
+        {
+            solu = temp;
+        }
+    }
+    cout << solu;
+
     return 0;
 }
+
