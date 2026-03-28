af8d33f2fe46c55f48f0387cdda23e66d0533159
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-03-28 20:45:00 +0530

    updated code

--- DSA_TRAINING/Kuriyama_Mirai.cpp
+++ DSA_TRAINING/Kuriyama_Mirai.cpp
@@ -2,25 +2,25 @@
 #include <algorithm>
 using namespace std;
 
-long long type_one(long a[], long start, long end)
-{
-    long long ans = 0;
-    for (long i = start; i <= end; i++)
-    {
-        ans += a[i];
-    }
-    return ans;
-}
+// long long type_one(long a[], long start, long end)
+// {
+//     long long ans = 0;
+//     for (long i = start; i <= end; i++)
+//     {
+//         ans += a[i];
+//     }
+//     return ans;
+// }
 
-long long type_two(long a[], long start, long end)
-{
-    long long ans = 0;
-    for (long i = start; i <= end; i++)
-    {
-        ans += a[i];
-    }
-    return ans;
-}
+// long long type_two(long a[], long start, long end)
+// {
+//     long long ans = 0;
+//     for (long i = start; i <= end; i++)
+//     {
+//         ans += a[i];
+//     }
+//     return ans;
+// }
 int main()
 {
     int n;
@@ -50,13 +50,17 @@ int main()
         long l, r;
         cin >> type >> l >> r;
         long long ans = 0;
+        int start = l - 1, end = r - 1;
         if (type == 1)
         {
-            ans = type_one(a, l - 1, r - 1);
+            // ans = type_one(a, l - 1, r - 1);
+            ans = abs(start - end) + 1 * a[start] + a[end] / 2;
         }
         else
         {
-            ans = type_two(sorted_Array, l - 1, r - 1);
+            // ans = type_two(sorted_Array, l - 1, r - 1);
+            ans = (abs(start - end) + 1) * (sorted_Array[start] + sorted_Array[end]) / 2;
+
         }
         solu[k++] = ans;
     }
