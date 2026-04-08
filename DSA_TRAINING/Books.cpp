f0f0262487f350aeb1a18dee28f3cf3123366ef1
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-04-08 19:29:39 +0530

    updated

--- DSA_TRAINING/Books.cpp
+++ DSA_TRAINING/Books.cpp
@@ -2,15 +2,23 @@
 using namespace std;
 
 int main() {
-    int n,t;
+    int n;
+    long long t;
     cin >> n >> t;
     int a[n];
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
     }
-
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if(t >= a[i]){
+            t -= a[i];
+            count++;
+        }
+    }
     
-    
+    cout << count;
     return 0;
 }
