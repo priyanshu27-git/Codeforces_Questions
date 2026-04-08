ad010880f9929be13c4374c2920c190df38d367d
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-04-08 21:11:04 +0530

    corrected code

--- DSA_TRAINING/Books.cpp
+++ DSA_TRAINING/Books.cpp
@@ -11,14 +11,36 @@ int main() {
         cin >> a[i];
     }
     int count = 0;
-    for (int i = 0; i < n; i++)
-    {
-        if(t >= a[i]){
-            t -= a[i];
+    // for (int i = 0; i < n; i++)
+    // {
+    //     if(t >= a[i]){
+    //         t -= a[i];
+    //         count++;
+    //     }
+    // }
+    
+    int left = 0, right = 0;
+    int k = t;
+    int current = 0, max = 0;
+    while(right < n){
+        if(k >= a[right]){
             count++;
+            k -= a[right];
+            right++;
+        }
+        else {
+            right = left++;
+            count = 0;
+            k = t;
+        }
+        current = count;
+        if(max < current){
+            max = current;
+        }
+        if(count == n){
+            break;
         }
     }
-    
-    cout << count;
+    cout << max;
     return 0;
 }
