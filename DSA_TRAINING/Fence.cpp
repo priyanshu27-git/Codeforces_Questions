4f9a3ede0cc3fd9b139feb2cd212b7b77b6fd40a
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-05-07 15:01:37 +0530

    update 2

--- DSA_TRAINING/Fence.cpp
+++ DSA_TRAINING/Fence.cpp
@@ -10,10 +10,11 @@ int main()
     {
         cin >> a[i];
     }
-    
+
     int count = 0;
     int currentsum = 0, minimumsum = 0;
     int index = 1;
+    
     for (int i = 0; i < n; i++)
     {
         if (count < k)
